fb2b3fc18c6166b5d8192ef8ce0c00c2f8f87cc9
Naleka <nalekabeckford@gmail.com>
2024-10-26 22:22:44 -0400

    Adding Edited tic-tac-toe.js to the repository

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,53 @@
+// Run code when the page has fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Select all div elements inside the board div
-    const squares = document.querySelectorAll("#board div");
-   
-    // Loop through each square
-    squares.forEach(square => {
-        // Add the square class to each div
-        square.classList.add("square");
-    });
-});     
+    // Find all the divs inside the element with id "board"
+   const squares = document.querySelectorAll("#board div"); 
+   let currentPlayer = "X"; // Track the current player
+   const boardState = Array(9).fill(" "); // Array to keep track of each square
+
+   // Loop through each square (each div inside #board)
+   squares.forEach((square, position) => {
+       // Add the "square" class to each div so it gets the correct styles from the CSS
+       square.classList.add("square");
+
+       // Attach the click event listener to each square
+       square.addEventListener("click", () => squareClick(position));
+   });
+
+   // Clicking a square 
+   function squareClick(position) {
+
+       // Check if square is empty
+       if (boardState[position]===" ") {
+
+           //Assign square position
+           const square = squares[position];
+
+           //Add the current player's mark
+           square.textContent = currentPlayer;
+
+           // Add the class 'X' or 'O' to  the square
+           square.classList.add(currentPlayer);
+
+           // update the board state array
+           boardState[position] = currentPlayer;
+
+           //switch to other player
+           currentPlayer= switchPlayer(currentPlayer);
+       }
+   }
+
+    // Switch to the other player
+   function switchPlayer (currentPlayer) {     
+       if (currentPlayer === "X") {
+           currentPlayer = "O";
+
+       } else {
+
+           currentPlayer = "X";
+       }
+       return currentPlayer;
+   }
+});
+
+
